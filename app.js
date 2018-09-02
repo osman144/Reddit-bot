1d62cfa1874d7ee27254f4986d5f419ad5e44ed5
osman144 <osman144@umn.edu>
2018-09-02 13:08:15 -0500

    reddit bot commit, app js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-require('env').config();
+require('dotenv').config();
 
+const Snoowrap = require('snoowrap');
 const Snoostorm = require('snoostorm');
-const Snoowrap = require ('snoowrap');
 
 // Build Snoowrap and Snoostorm clients 
 
 const r = new Snoowrap ( {
-    userAgent: 'soccer-bot',
+    userAgent: 'Reddit-bot',
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     username: process.env.REDDIT_USER,
@@ -17,8 +17,8 @@ const client = new Snoostorm(r);
 
 // Configure options for stream: subreddit & results per query
 const streamOpts = {
-    subreddit: 'all',
-    results: 25
+    subreddit: 'testingground4bots',
+    results: 2
 };
 
 // Create a Snoostorm CommentStream with the specified options
@@ -26,6 +26,10 @@ const comments = client.CommentStream(streamOpts);
 
 // On comment, perform whatever logic you want to do
 comments.on('comment', (comment) => {
-    console.log(comment);
+    if (comment.body === ':(') {
+        comment.reply(':)');
+    }
+
+    // console.log(comment.body)
 });
   
